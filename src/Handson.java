67cfa022695973d85175e4684b0c2909d211da03
Your Name <you@example.com>
2022-01-17 21:24:09 -0500

    convert to queue to stack

--- src/Handson.java
+++ src/Handson.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,21 +9,13 @@ public class Handson {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		
-		Scanner scanner = new Scanner(new File("student.txt"));
-		List<String> students = new ArrayList<String>();
-		
-		while(scanner.hasNextLine()) {
-			students.add(scanner.nextLine());
-		}
-		
-		for(int i=0;i<students.size();i++) {
-			System.out.println("Name: "+ students.get(i));
-		}
-		
-		scanner.close();
-
-
+		LinkedList<String> names = new LinkedList<String>();
+		names.push("Sam");
+		names.push("Kwon");
+		names.push("Bolden");
+		System.out.println(names.pop());
+		System.out.println(names.remove());
+		System.out.println(names.removeFirst());
 
 	}
 
