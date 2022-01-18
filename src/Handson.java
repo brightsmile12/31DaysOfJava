cb1364fe49a222737693ea98c42939cfb7cd2c4b
Your Name <you@example.com>
2022-01-17 19:15:32 -0500

    add input from file.

--- src/Handson.java
+++ src/Handson.java
@@ -1,9 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Handson {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		
+		Scanner scanner = new Scanner(new File("student.txt"));
+		List<String> students = new ArrayList<String>();
+		
+		while(scanner.hasNextLine()) {
+			students.add(scanner.nextLine());
+		}
+		
+		for(int i=0;i<students.size();i++) {
+			System.out.println("Name: "+ students.get(i));
+		}
+		
+		scanner.close();
+
+
 
 	}
 
